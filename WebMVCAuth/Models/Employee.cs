20e1b0e89357f7421b474ffeb73dc11c8ba863e1
Keqing Li <keqingli1129@gmail.com>
2025-10-16 22:15:41 -0500

    add data model

---
+++ WebMVCAuth/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebMVCAuth.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateTime? HireDate { get; set; }
+
+    public double? Salary { get; set; }
+}
